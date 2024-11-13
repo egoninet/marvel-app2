42beb070b5fb399575226f7680968cd9ab55cb91
egoninet <eliott.goninet@etu.univ-poitiers.fr>
2024-11-13 17:18:57 +0100

    new-marvel-app

---
+++ src/Layout.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Outlet } from 'react-router';
+import { NavLink } from 'react-router-dom';
+import  {version} from '../package.json';
+const Layout = () => {
+    return (
+        <>
+            <header>
+                <h1>Marvel App</h1>
+                <nav>
+                    <NavLink to="/">Home</NavLink> - <NavLink to="/about">About</NavLink> - <NavLink to="/contact">Contact</NavLink>
+                </nav>
+            </header>
+            <main>
+                <Outlet />
+            </main>
+            <footer>
+                <p>Marvel App - 2024 - {version}</p>
+            </footer>
+        </>
+    );
+};
+export default Layout;
